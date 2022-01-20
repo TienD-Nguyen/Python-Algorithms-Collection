22543ab2d0d3dbef5956e7f1a5695f25cbae68f7
TienD-Nguyen <tien.d.nguyen@outlook.com>
2022-01-20 18:22:31 +1100

    Edited and added a recursive method

--- Sorting/Bubble_Sort.py
+++ Sorting/Bubble_Sort.py
@@ -1,4 +1,4 @@
-def selection_sort(arr=[]):
+def bubble_sort(arr=[]):
 
     n = len(arr)
     for i in range(n):
@@ -9,8 +9,21 @@ def selection_sort(arr=[]):
     return arr
 
 
+def recursive_bubble_sort(arr=None, arr_len=None):
+    if arr_len == 1:
+        return
+
+    for i in range(arr_len-1):
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+
+    recursive_bubble_sort(arr, arr_len - 1)
+    return arr
+
 
 if __name__ == "__main__":
     unsorted_arr = [9, 3, 5, 1, 2, 10, 69, 29, 44]
-    sorted_arr = selection_sort(unsorted_arr)
+    sorted_arr = bubble_sort(unsorted_arr)
+    recursive_sorted_arr = recursive_bubble_sort(unsorted_arr, len(unsorted_arr))
     print(sorted_arr)
+    print(recursive_sorted_arr)
